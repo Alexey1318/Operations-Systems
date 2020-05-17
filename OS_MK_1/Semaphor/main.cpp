4adf47d7456789ff187fc8511e646de9a711dedb
Alexey Nikolskyi <alexey49035@gmail.com>
2020-05-17 22:11:01 +0300

    i haven't semaphor code yet

--- OS_MK_1/Semaphor/main.cpp
+++ OS_MK_1/Semaphor/main.cpp
@@ -1,6 +1,15 @@
 #include <sys/sem.h>
+#include <stdio.h>
+
+// Функция открытия существующего семафора
+void open_sem(int *sid, key_t key){
+    if((*sid = semget(key, 0, 0666)) == -1){
+        printf("Семафор не существует\n");
+        return;
+    }
+}
 
 int main(int argc, char*argv[]){
-
+    
     return 0;
 }
